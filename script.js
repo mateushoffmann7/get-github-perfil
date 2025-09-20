51bede7fe26c09b79e0699742a6fc4b2636c8e29
Mettel07 <mateushoffmann0707@gmail.com>
2025-09-20 12:20:59 -0300

    create function for add imgProfile

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //https://avatars.githubusercontent.com/u/184573664?v=4
 //https://api.github.com/users/mateushoffmann7
+const input = document.querySelector(".inputSearch");
+const sayHello = document.querySelector(".textHello");
+const profileImg = document.querySelector(".imgPerfil img");
+const list = document.querySelector(".list");
+
+const handleAddPerfil = async (e) => {
+  let response = await fetch(`https://api.github.com/users/${input.value}`);
+  let json = await response.json();
+  if (json.login === undefined) {
+    throw new Error("");
+  }
+  if (input.value !== "" && e.key === "Enter") {
+    profileImg.setAttribute("src", json.avatar_url);
+  }
+};
+
+document.documentElement.addEventListener("keyup", handleAddPerfil);
