606a8916fd35cd77390bc9d2152bc6dcd2e4b02f
Mettel07 <mateushoffmann0707@gmail.com>
2025-09-22 12:02:34 -0300

    adjust user not found

--- script.js
+++ script.js
@@ -4,31 +4,37 @@ const profileImg = document.querySelector(".imgPerfil img");
 const list = document.querySelector(".list");
 
 const handleAddPerfil = async (e) => {
-  let response = await fetch(`https://api.github.com/users/${input.value}`);
-  let json = await response.json();
-  if (response.ok && json.login) {
+  try {
+    let response = await fetch(`https://api.github.com/users/${input.value}`);
+    let json = await response.json();
+    if (!response) {
+      throw new Error("");
+    }
     if (input.value !== "" && e.key === "Enter") {
-      list.innerHTML = "";
-      sayHello.innerHTML =
-        json.name === null ? "---" : `Olá, me chamo ${json.name}!`;
-      profileImg.setAttribute("src", json.avatar_url);
+      sayHello.innerHTML = !json.name
+        ? "Usuário não encontrado..."
+        : `Olá, me chamo ${json.name}!`;
+      profileImg.setAttribute(
+        "src",
+        !json.avatar_url
+          ? "./assets/Eric Bellinger's 5 Tips On Making It Big As A Songwriter.jpg"
+          : json.avatar_url
+      );
+      input.classList.remove("error");
       const newBio = document.createElement("li");
       newBio.classList.add("newLi");
-      newBio.innerHTML = json.bio === null ? "---" : `Bio: ${json.bio}`;
+      newBio.innerHTML = !json.bio ? "---" : `Bio: ${json.bio}`;
       const newLocation = document.createElement("li");
       newLocation.classList.add("newLi");
-      newLocation.innerHTML =
-        json.location === null ? "---" : `Localização: ${json.location}`;
+      newLocation.innerHTML = !json.location
+        ? "---"
+        : `Localização: ${json.location}`;
       list.append(newBio);
       list.append(newLocation);
       input.value = "";
-    } else {
-      list.innerHTML = "";
-      profileImg.setAttribute(
-        "src",
-        "./assets/Eric Bellinger's 5 Tips On Making It Big As A Songwriter.jpg"
-      );
     }
+  } catch (error) {
+    input.classList.add("error");
   }
 };
 
